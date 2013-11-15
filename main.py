d11f291969dbe5adf54537f1d54face048e40732
konjac <cs.huangkun@gmail.com>
2013-11-15 14:43:11 +0800

    output as JSON format

--- main.py
+++ main.py
@@ -25,33 +25,29 @@ login.open(email, password)
 html = urllib2.urlopen('http://3g.renren.com/friendlist.do').read()
 page_cnt = 0
 person_cnt = 0
+f.write("[");
 while True:
     profiles = map(lambda t : t.attrib['href'], jquery.query(html, "a[class=p]"))
-    f.write("page #" + str(page_cnt) + "\n")
     for p in profiles:
         url = p.replace("profile", "details")
+        uid = re.findall(r"id=([0-9]+)", url)[0]
+        print uid, person_cnt
         detail_page = urllib2.urlopen(url).read()
         name = jquery.query(detail_page, ".sec b a").text()
         res = jquery.query(detail_page, ".list")
         info = "\n".join(res.listOuterHtml());
         if len(info) == 0:
             info = u"null"
-        print name
-        for s in re.findall(ur"[^>]*：[^<]*", info):
-            print s
-        #print info
-        f.write("--------------------------------------------\n")
-        f.write("ID #" + str(person_cnt) + "\n")
-        f.write(util.utf8_wrapper(name))
-        f.write("\n")
+        if person_cnt>0:
+            f.write(",");
+        f.write("{\n");
+        f.write(("\t\"%s\": %d")%("uid", int(uid)))
+        f.write((",\n\t\"%s\": \"%s\"")%("Name", util.utf8_wrapper(name)))
         for s in re.findall(ur"[^>]*：[^<]*", info):
             idx = s.find(u"：");
-            s = s[0:idx] + "\t" + s[idx+1:]
-            f.write(util.utf8_wrapper(s))
-            f.write("\n")
-        #f.write(util.utf8_wrapper(info))
+            f.write(util.utf8_wrapper((",\n\t\"%s\": \"%s\"")%(s[0:idx], s[idx+1:])))
         time.sleep(1)
-        f.write("\n")
+        f.write("\n}")
         f.flush()
         person_cnt = person_cnt + 1
 
@@ -62,3 +58,5 @@ while True:
     html = urllib2.urlopen(next_page).read()
     page_cnt = page_cnt + 1
     time.sleep(1)
+f.write("]\n")
+f.close()
