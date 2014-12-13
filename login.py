32a6392c23777ee494ebfa552a2dc094f5fc55d7
konjac <cs.huangkun@gmail.com>
2014-12-13 15:47:31 +0800

    verfify code may be unnecessary

--- login.py
+++ login.py
@@ -14,18 +14,21 @@ def open(email, password):
     urllib2.install_opener(opener)
 
     html = urllib2.urlopen('http://m.renren.com').read()
-    img = jquery.query(html, "img")[1].attrib['src'];
     
-    verifykey = jquery.query(html, "input[name=verifykey]")[0].attrib['value']
-    
-    print img
-    verifycode = raw_input("please visit the above url in your browser, and type the verify code on the page:\n")
-
+    img, verifykey = None, None 
     xn = {}
     xn['email'] = email
     xn['password'] = password
-    xn['verifycode'] = verifycode
-    xn['verifykey'] = verifykey
+    try:
+        img = jquery.query(html, "img")[1].attrib['src'];
+        verifykey = jquery.query(html, "input[name=verifykey]")[0].attrib['value']
+        print img
+        verifycode = raw_input("please visit the above url in your browser, and type the verify code on the page:\n")
+        xn['verifycode'] = verifycode
+        xn['verifykey'] = verifykey
+    except:
+        pass
+
     #print xn
     
     data = urllib.urlencode(xn)
